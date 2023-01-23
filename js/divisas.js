2116fe6475b26abf439b2763da9f36a74c65108a
Iñaki Tuda <inakituda@gmail.com>
2023-01-23 12:05:21 -0300

    arme nuevo repositorio

---
+++ js/divisas.js
@@ -0,0 +1,38 @@
+const productos =
+[
+    {
+        id: 1,
+        nombre: "Dolares",
+        tipo: "Dólar",
+        descripcion: "Dólar Estadounidense",
+        precio: 186.82,
+        cantidad:0
+    },
+
+    {
+        id: 2,
+        nombre: "Euros",
+        tipo: "Euro",
+        descripcion: "Euro",
+        precio: 190.78,
+        cantidad:0
+    },
+
+    {
+        id: 3,
+        nombre: "Reales",
+        tipo: "Real",
+        descripcion: "Real Brasilero",
+        precio: 34.12,
+        cantidad:0
+    },
+
+    {
+        id: 4,
+        nombre: "Yenes",
+        tipo: "Yen",
+        descripcion: "Yen Japones",
+        precio: 106.30,
+        cantidad:0
+    },
+]
---
+++ js/main.js
@@ -0,0 +1,21 @@
+const formulario = document.getElementById("form");
+
+const nombre = document.getElementById("firstName");
+const apellido = document.getElementById("lastName");
+const email = document.getElementById("email");
+const monto = document.getElementById("amount");
+const de = document.getElementById("feesde");
+const a = document.getElementById("feesa");
+
+const montoFinal = document.getElementById("finalAmount");
+const preciode = document.getElementById("finalFeesde");
+const precioa = document.getElementById("finalFeesa");
+const totalADevolver = document.getElementById("totalAmount");
+
+const peso = 1
+const dolar = 186.82;
+const euro = 190.78;
+const real = 34.12;
+const yen = 106.30;
+
+
